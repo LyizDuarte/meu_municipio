0cf0aeda0d20befbfe5f065a2d211571cbcbdc91
LyizDuarte <looduarte21@gmail.com>
2025-10-24 21:27:16 -0300

    feat(routes): add posts routes to index router

--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -3,8 +3,10 @@ const router = express.Router();
 
 const authRoutes = require('./auth');
 const healthRoutes = require('./health');
+const postRoutes = require('./posts');
 
 router.use('/auth', authRoutes);
 router.use('/health', healthRoutes);
+router.use('/posts', postRoutes);
 
 module.exports = router;
