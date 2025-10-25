3ecfe21d32181165c925802bfaf96514a36c990a
LyizDuarte <looduarte21@gmail.com>
2025-10-24 21:55:08 -0300

    docs: add swagger documentation endpoint
    Add Swagger UI documentation endpoint at /api/docs to improve API documentation accessibility

--- backend/src/app.js
+++ backend/src/app.js
@@ -18,7 +18,12 @@ app.use('/uploads', express.static('src/uploads'));
 
 // Rota principal
 const routes = require('./routes');
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('./docs/swagger');
 app.use('/api', routes);
+
+// Documentação Swagger
+app.use('/api/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 // Exporta o app (não inicia o servidor aqui)
 module.exports = app;
